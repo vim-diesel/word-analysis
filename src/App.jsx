29a19090c7f6a61376dedf59b318148c15cbdd26
Ian C <ian.clark@hotmail.com>
2024-05-14 17:02:34 +0200

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import Footer from './Footer';
 import Header from './Header';
 import { Toaster } from 'react-hot-toast';
 
-// TODO:
-// 1. Add media queries to make the app responsive.
-
 function App() {
   return (
     <>
